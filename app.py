bb587cdc9b950c5f318633582e08b9a6068d2da2
james sebastian <jamessebastian916@gmail.com>
2023-04-14 06:18:58 -0400

    pages2

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import time
 
 import redis
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
@@ -20,4 +20,27 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+    return ' <a href="/">1st page</a><a href="/1">2nd page</a><a href="/2">3rd page</a> hit-- {} times.\n'.format(count)
+
+@app.route('/1')
+def hello1():
+    count = get_hit_count()
+    return ' <a href="/">1st page</a><a href="/1">2nd page</a><a href="/2">3rd page</a> hit-- {} times.\n'.format(count)
+
+@app.route('/2')
+def hello2():
+    count = get_hit_count()
+    return ' <a href="/">1st page</a><a href="/1">2nd page</a><a href="/2">3rd page</a> hit-- {} times.\n'.format(count)
+
+
+@app.route('/page1')
+def index():
+    return render_template('index.html')
+
+@app.route('/page2')
+def page1():
+    return render_template('page1.html')
+
+@app.route('/page3')
+def page2():
+    return render_template('page2.html')
